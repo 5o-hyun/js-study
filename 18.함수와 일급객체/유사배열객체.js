93cbf96d709d88f41542b948bd70cd23d619fd29
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2022-12-18 23:25:39 +0900

    Feat: 유사배열객체

---
+++ 18.함수와 일급객체/유사배열객체.js
@@ -0,0 +1,8 @@
+const boxes = document.querySelectorAll(".box");
+
+console.log(boxes);
+
+const elementIndex = 9;
+const elementValue = boxes[elementIndex];
+
+console.log(elementValue.textContent);
